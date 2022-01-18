81dca7fcc9233fe213b6124433adc76a1485b48f
Nathan Petrangelo <nathan@petrangelo.org>
2022-01-18 01:52:03 -0500

    Added contributions.sql

---
+++ server/api/schema/contributions.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS contributions;
+
+CREATE TABLE contributions(
+    id              SERIAL PRIMARY KEY NOT NULL,
+    author_id       INT,
+    title_id        INT,
+    what            TEXT,
+    created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX index_contributions_on_author_id_and_title_id ON contributions(author_id, title_id);
+CREATE INDEX index_contributions_on_author_id ON contributions(author_id);
+CREATE INDEX index_contributions_on_title_id ON contributions(title_id);
