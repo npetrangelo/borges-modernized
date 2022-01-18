47d74f87467e2b9bd691a1629400f62bed9f420c
Nathan Petrangelo <nathan@petrangelo.org>
2022-01-18 02:17:09 -0500

    Change big varchar to text

--- server/api/schema/authors.sql
+++ server/api/schema/authors.sql
@@ -7,5 +7,5 @@ CREATE TABLE authors (
     created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     full_name       TEXT,
-    bio             VARCHAR(16777215)
+    bio             TEXT
 );
